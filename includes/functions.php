b42ce2a657e3bf8941dfc412ce45895ca89f1445
randie20 <luttarandy254@gmail.com>
2026-08-15 11:07:13 +0300

    Initialize application sessions

--- includes/functions.php
+++ includes/functions.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../config/database.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 /*
 |--------------------------------------------------------------------------
 | Application URL
